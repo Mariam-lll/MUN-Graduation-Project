0529b58d63357d1278859eb30f3e45590c2bcc21
Mariam Ahmed <463178750sub@gmail.com>
2026-05-18 23:16:46 +0300

    Edited Task3.py

--- Task3.py
+++ Task3.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import os
 
 def load_file():
     while True:
@@ -8,7 +10,7 @@ def load_file():
             break
         except:
             print("File not found. Please enter a valid file path.")
-    return read_file
+    return read_file, file_path
 
 def words_lines(file):
     words = file.split()
@@ -23,19 +25,23 @@ def file_report(file):
     words, lines = words_lines(file)
     print(f"~File report~\nWords count: {words}\nLines count: {lines}\nNumber of characters: {len(file)}")
 
+def last_modified(file_path):
+    mod_date = os.path.getmtime(file_path)
+    return datetime.fromtimestamp(mod_date)
+
 
 def start():
     print("Welcome to file reporter")
-    file = load_file()
-    print("How can we help?\n1. Words and line count\n2. The frequency of a word\n3. Show file report\n4. Preview file")
+    file, file_path = load_file()
+    print("How can we help?\n1. Words and line count\n2. The frequency of a word\n3. Show file report\n4. Preview file\n5. Last modification date")
 
     while True:
         service = input("Please enter the service number: ")
         try: 
-            int(service) and service in ["1", "2", "3", "4"]
+            int(service) and service in ["1", "2", "3", "4", "5"]
             break
         except:
-            print("Please enter a valid service number within 1, 4.")
+            print("Please enter a valid service number within 1, 5.")
 
     if service == "1":
         words, lines = words_lines(file)
@@ -47,5 +53,8 @@ def start():
         file_report(file)
     elif service == "4":
         print(file)
+    elif service == "5":
+        print(f"The last modification date is: {last_modified(file_path)}")
+    
 start()
-
+print("Thanks for using file reporter!!")
